69005d2c4f2955ec596e2158001b6a48100342d6
Victor Pereira <victor.pereira@bigrails.com>
2010-09-16 20:54:05 +0200

    first commit

---
+++ config.h
@@ -0,0 +1,28 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/*
+ * $Id: config.h.in,v 1.5 2006/11/28 13:36:02 jpr5 Exp $
+ *
+ * Copyright (c) 2006  Jordan Ritter <jpr5@darkridge.com>
+ *
+ * Please refer to the LICENSE file for more information.
+ *
+ */
+
+#define USE_PCRE 0
+#define USE_TRE 1
+#define USE_IPv6 0
+
+#define HAVE_DLT_RAW 0
+#define HAVE_DLT_LOOP 0
+#define HAVE_DLT_LINUX_SLL 0
+#define HAVE_DLT_IEEE802_11 0
+#define HAVE_DLT_IEEE802_11_RADIO 0
+
+#define HAVE_DUMB_UDPHDR 0
+
+#define USE_PCAP_RESTART 0
+#define PCAP_RESTART_FUNC unused
+
+#define USE_DROPPRIVS 1
+#define DROPPRIVS_USER "nobody"
+
